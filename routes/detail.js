d9c4a96a1fc6689c157adfe36b940dd940a44ff6
d.bitter <d.bitter@viriciti.nl>
2017-03-20 16:12:05 +0100

    getting more details for movie on detailpage (reverted from commit eafd95ea54f62bc1aaec4b5e133a6e8ca6326ff2) (reverted from commit b56189f17ff34d177fba3fe99be097fd8435040c)

--- routes/detail.js
+++ routes/detail.js
@@ -1,17 +1,16 @@
 const express = require('express')
-const lockr = require('lockr')
+const unirest = require('unirest')
 
 const router = express.Router()
 
 router.get('/:id', (req, res) => {
 	const id = req.params.id
-	const movies = lockr.get('movies')
 
-	const movie = movies.find(function(result) {
-		return result.id == id
-	});
-
-	res.render('components/detail', {movie})
+	unirest.get('http://api.themoviedb.org/3/movie/' + id + '?api_key=' + process.env.APIKEY)
+		.end(function (response) {
+			const movie = response.body
+			res.render('components/detail', {movie})
+		})
 })
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,12 @@
 const express = require('express')
 const unirest = require('unirest')
-const lockr = require('lockr')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
 	unirest.get('http://api.tmdb.org/3/discover/movie?api_key=' + process.env.APIKEY + '&sort_by=popularity.desc')
 		.end(function (response) {
-			// create localStorage if it's not there
-			//  TODO: this looks hacky
-			if (typeof localStorage === "undefined" || localStorage === null) {
-				const LocalStorage = require('node-localstorage').LocalStorage;
-				localStorage = new LocalStorage('./scratch');
-			}
-
 			const movies = response.body.results
-
-			lockr.set('movies', movies);
-
 			res.render('components/overview', {movies})
 		})
 })
