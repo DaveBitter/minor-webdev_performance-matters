8d074703a27c4e6c3623a429c8720831e506ab8c
d.bitter <d.bitter@viriciti.nl>
2017-03-20 16:34:21 +0100

    formatting data

--- routes/detail.js
+++ routes/detail.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const unirest = require('unirest')
+const currencyFormatter = require('currency-formatter')
+const moment = require('moment')
 
 const router = express.Router()
 
@@ -8,9 +10,20 @@ router.get('/:id', (req, res) => {
 
 	unirest.get('http://api.themoviedb.org/3/movie/' + id + '?api_key=' + process.env.APIKEY)
 		.end(function (response) {
-			const movie = response.body
+			const movie = formatMovie(response.body)
 			res.render('components/detail', {movie})
 		})
 })
+
+const formatMovie = (movie) => {
+	movie.budget = currencyFormatter.format(movie.budget, { code: 'USD' })
+	movie.revenue = currencyFormatter.format(movie.revenue, { code: 'USD' })
+	movie.release_date = moment(movie.release_date).format('MMMM Do YYYY')
+	movie.runtime = movie.runtime + ' minutes'
+	movie.poster_path = 'http://image.tmdb.org/t/p/w500/' + movie.poster_path
+	movie.imdb_link = 'http://imdb.com/title/' + movie.id
+
+	return movie
+}
 
 module.exports = router
