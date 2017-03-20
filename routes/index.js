8d0c3b0b78ae64b7e69493cf2dd3627e58cd425b
d.bitter <d.bitter@viriciti.nl>
2017-03-20 14:35:31 +0100

    loading in data from API and rendering overviewpage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require('express')
+const unirest = require('unirest')
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/', (req, res) => {
-	res.render('components/hello')
+	unirest.get('http://api.tmdb.org/3/discover/movie?api_key=' + process.env.APIKEY + '&sort_by=popularity.desc')
+		.end(function (response) {
+			const movies = response.body.results
+			res.render('components/overview', {movies})
+		})
 })
 
-module.exports = router;
+module.exports = router
