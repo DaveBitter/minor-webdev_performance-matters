63eb7ff93274e9731e5c62ae482a99f74b5051c4
d.bitter <d.bitter@viriciti.nl>
2017-03-29 11:54:48 +0200

    cleanup on code

--- clientside/app.js
+++ clientside/app.js
@@ -1,16 +1,16 @@
-if ( 'serviceWorker' in navigator ) {
+if ('serviceWorker' in navigator) {
 
-    navigator.serviceWorker
+	navigator.serviceWorker
 
-        navigator.serviceWorker.register('/sw.js')
+	navigator.serviceWorker.register('/sw.js')
 
-        .then(function (registration) {
-            console.log("Service worker Registered at ", registration)
-        })
+		.then(function(registration) {
+			console.log("Service worker Registered at ", registration)
+		})
 
-        .catch(function (err) {
-            console.log("Service worker failed to register at", err)
-        })
+		.catch(function(err) {
+			console.log("Service worker failed to register at", err)
+		})
 
 }
 
--- clientside/renderData.js
+++ clientside/renderData.js
@@ -1,7 +1,49 @@
-const renderData = (data) => {
-	// create html from template
-	// render
-	return data
+const renderData = (movie) => {
+	const movieModal = document.querySelector('#movie-modal')
+	const movieModalContent = document.querySelector('#movie-modal-content')
+
+	const template = [
+		'<article>',
+		'<header>',
+		'<h1>' + movie.title + '</h1>',
+		'<sup> ' + movie.tagline + ' </sup>',
+		'</header>',
+		'<hr>',
+		'<div class="content">',
+		'<img src=' + movie.poster_path + '>',
+		'<div class="info">',
+		'<h3>Summary</h3>',
+		'<p>' + movie.overview + '</p>',
+		'<ul>',
+		'<li>',
+		'<h4>Budget (USD)</h4>',
+		'<p>' + movie.budget + '</p>',
+		'</li>',
+		'<li>',
+		'<h4>Revenue (USD)</h4>',
+		'<p>' + movie.revenue + '</p>',
+		'</li>',
+		'<li>',
+		'<h4>Release date</h4>',
+		'<p>' + movie.release_date + '</p>',
+		'</li>',
+		'<li>',
+		'<h4>Runtime</h4>',
+		'<p>' + movie.runtime + '</p>',
+		'</li>',
+		'<li>',
+		'<h4>Check on IMDB</h4>',
+		'<a>' + movie.imdb_link + '</a>',
+		'</li>',
+		'</ul>',
+		'</div>',
+		'</div>',
+		'</article>'
+	].join("\n");
+
+	movieModalContent.innerHTML = template
+	movieModal.classList.add('open')
 }
+
 
 module.exports = renderData
